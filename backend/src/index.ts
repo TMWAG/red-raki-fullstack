5237568484f2e5979ff62de4bc5047dd7a8e6639
TMWAG <semyon.valyaev@gmail.com>
2023-09-27 22:30:27 +0300

    зарегистрированы маршруты, декораторы и модуль JWT

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,29 @@
 import fastify from "fastify";
-import userRoutes from "./modules/user/user.routes";
+import routes from "./modules";
+import fjwt from "@fastify/jwt";
+import auth from "./decorators/auth";
+import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 
-const app = fastify({
-  logger: true,
-});
+export const app = fastify({
+	logger: true,
+}).withTypeProvider<TypeBoxTypeProvider>();
 
-app.register(userRoutes, {prefix: '/user'})
+app.register(fjwt, { secret: String(process.env.JWT_SALT) });
 
-app.listen({
-  port: 3001,
-  host: "0.0.0.0"
-}, (err, address) => {
-  if(err){
-    app.log.error(err);
-    process.exit(1);
-  }
-  console.log(`server is listening on ${address}`);
-});
+app.register(auth);
+
+app.register(routes);
+
+app.listen(
+	{
+		port: 3001,
+		host: "0.0.0.0",
+	},
+	(err, address) => {
+		if (err) {
+			app.log.error(err);
+			process.exit(1);
+		}
+		console.log(`server is listening on ${address}`);
+	}
+);
