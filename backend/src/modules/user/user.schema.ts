e29ab899a4ec146836eeb231b620a2f54d12f806
TMWAG <semyon.valyaev@gmail.com>
2023-09-27 22:22:37 +0300

    схемы для взаимодействия с пользователями

---
+++ backend/src/modules/user/user.schema.ts
@@ -0,0 +1,36 @@
+import { UserRoles } from "@prisma/client";
+import { Type, Static } from "@sinclair/typebox";
+
+export const createUserSchema = Type.Object({
+  phone: Type.RegExp(/^((\+7|7|8)+([0-9]){10})$/),
+	password: Type.String()
+});
+export const createUserResponseSchema = Type.Object({
+  id: Type.String(),
+  phone: Type.String(),
+  role: Type.Enum(UserRoles),
+});
+
+export const loginUserSchema = Type.Object({
+  phone: Type.String(),
+  password: Type.String(),
+})
+
+export const loginUserResponseSchema = Type.Object({
+  phone: Type.String(),
+  role: Type.Enum(UserRoles),
+  token: Type.String(),
+})
+
+export const getAllUsersResponseSchema = Type.Object({
+  count: Type.Integer(),
+  users: Type.Array(Type.Object({
+    id: Type.String(),
+    name: Type.Optional(Type.String()),
+    phone: Type.String(),
+    role: Type.Enum(UserRoles),
+  })),
+});
+
+export type CreateUserInput = Static<typeof createUserSchema>;
+export type LoginUserInput = Static<typeof loginUserSchema>;
