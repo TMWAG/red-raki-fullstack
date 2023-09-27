2d30de9f15a0da95349b56a443776aba697ea6ce
TMWAG <semyon.valyaev@gmail.com>
2023-09-27 22:25:25 +0300

    маршруты для регистрации, логина и получения всех пользователей

--- backend/src/modules/user/user.routes.ts
+++ backend/src/modules/user/user.routes.ts
@@ -1,9 +1,42 @@
 import { FastifyInstance } from "fastify";
-import { registerUserHandler } from "./user.service";
+import { getAllUsersHandler, loginUserHandler, registerUserHandler } from "./user.service";
+import { createUserResponseSchema, createUserSchema, getAllUsersResponseSchema, loginUserResponseSchema, loginUserSchema } from "./user.schema";
 
 export default async function userRoutes(app: FastifyInstance) {
+  app.post(
+    '/register',
+    {
+      schema: { 
+        body: createUserSchema,
+        response: {
+          201: createUserResponseSchema,
+        },
+      },
+    },
+    registerUserHandler,
+  );
+  app.post(
+    '/login',
+    {
+      schema: {
+        body: loginUserSchema,
+        response: {
+          200: loginUserResponseSchema,
+        },
+      },
+    },
+    loginUserHandler,
+  );
   app.get(
-    '/',
-    registerUserHandler
-  )
+    '/all',
+    {
+      preHandler: [app.authorizeAdmin],
+      schema: {
+        response: {
+          200: getAllUsersResponseSchema,
+        }
+      }
+    },
+    getAllUsersHandler,
+  );
 }
