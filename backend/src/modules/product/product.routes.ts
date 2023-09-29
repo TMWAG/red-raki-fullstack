cdcaf810906ae3dad879bd02860907ff9a014dbe
TMWAG <semyon.valyaev@gmail.com>
2023-09-30 01:41:22 +0300

    изменены типы, убраны схемы для тел запросов POST и PATCH

--- backend/src/modules/product/product.routes.ts
+++ backend/src/modules/product/product.routes.ts
@@ -1,68 +1,87 @@
 import { FastifyInstance } from "fastify";
-import { createProductHandler, deleteProductHandler, getAllProductsHandler, getProductHandler, updateProductHandler } from "./product.service";
-import { createProductInput, createProductResponseSchema, createProductSchema, deleteProductInput, deleteProductResponseSchema, deleteProductSchema, getProductInput, getProductResponseSchema, getProductSchema, getProductsResponseSchema, updateProductInput, updateProductResponseSchema, updateProductSchema } from "./product.schema";
+import {
+	createProductHandler,
+	deleteProductHandler,
+	getAllProductsHandler,
+	getProductHandler,
+	updateProductHandler,
+} from "./product.service";
+import {
+	createProductResponseSchema,
+	deleteProductInput,
+	deleteProductResponseSchema,
+	deleteProductSchema,
+	getProductInput,
+	getProductResponseSchema,
+	getProductSchema,
+	getProductsResponseSchema,
+	updateProductResponseSchema,
+} from "./product.schema";
+import { IProductInput } from "../../@types";
+import fastifyMultipart from "@fastify/multipart";
 
 export default async function productRoutes(app: FastifyInstance) {
-  app.post<{ Body: createProductInput }>(
-    '/',
-    {
-      preHandler: [app.authorizeAdminOrSupervisor],
-      schema: {
-        body: createProductSchema,
-        response: {
-          201: createProductResponseSchema,
-        },
-      },
-    },
-    createProductHandler
-  );
-  app.get(
-    '/',
-    {
-      schema: {
-        response: {
-          200: getProductsResponseSchema,
-        },
-      },
-    },
-    getAllProductsHandler,
-  );
-  app.get<{ Params: getProductInput }>(
-    '/:id',
-    {
-      schema: {
-        params: getProductSchema,
-        response: {
-          200: getProductResponseSchema,
-        },
-      },
-    },
-    getProductHandler,
-  );
-  app.patch<{ Body: updateProductInput }>(
-    '/',
-    {
-      preHandler: [app.authorizeAdminOrSupervisor],
-      schema: {
-        body: updateProductSchema,
-        response: {
-          200: updateProductResponseSchema,
-        },
-      },
-    },
-    updateProductHandler,
-  );
-  app.delete<{ Params: deleteProductInput }>(
-    '/:id',
-    {
-      preHandler: [app.authorizeAdminOrSupervisor],
-      schema: {
-        params: deleteProductSchema,
-        response: {
-          200: deleteProductResponseSchema,
-        },
-      },
-    },
-    deleteProductHandler,
-  );
+	app.register(fastifyMultipart, { attachFieldsToBody: true });
+	app.post<{
+		Body: IProductInput;
+	}>(
+		"/",
+		{
+			preHandler: [app.authorizeAdminOrSupervisor],
+			schema: {
+				response: {
+					201: createProductResponseSchema,
+				},
+			},
+		},
+		createProductHandler
+	);
+	app.get(
+		"/",
+		{
+			schema: {
+				response: {
+					200: getProductsResponseSchema,
+				},
+			},
+		},
+		getAllProductsHandler
+	);
+	app.get<{ Params: getProductInput }>(
+		"/:id",
+		{
+			schema: {
+				params: getProductSchema,
+				response: {
+					200: getProductResponseSchema,
+				},
+			},
+		},
+		getProductHandler
+	);
+	app.patch<{ Body: IProductInput; Params: { id: string } }>(
+		"/:id",
+		{
+			preHandler: [app.authorizeAdminOrSupervisor],
+			schema: {
+				response: {
+					200: updateProductResponseSchema,
+				},
+			},
+		},
+		updateProductHandler
+	);
+	app.delete<{ Params: deleteProductInput }>(
+		"/:id",
+		{
+			preHandler: [app.authorizeAdminOrSupervisor],
+			schema: {
+				params: deleteProductSchema,
+				response: {
+					200: deleteProductResponseSchema,
+				},
+			},
+		},
+		deleteProductHandler
+	);
 }
