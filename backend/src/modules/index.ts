deb5148992a02d0665dfe21d7d8b60afd4155f9a
TMWAG <semyon.valyaev@gmail.com>
2023-09-29 21:52:56 +0300

    зарегистрирована ветка /products

--- backend/src/modules/index.ts
+++ backend/src/modules/index.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 import userRoutes from "./user/user.routes";
 import categoryRoutes from "./category/category.routes";
+import productRoutes from "./product/product.routes";
 
 export default async function routes(app: FastifyInstance) {
 	app.register(userRoutes, { prefix: "/user" });
 	app.register(categoryRoutes, { prefix: "/category" });
+	app.register(productRoutes, { prefix: "/product" });
 }
