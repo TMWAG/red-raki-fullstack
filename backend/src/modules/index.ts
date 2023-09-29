bb2a25b6a79deda3c307663d7e02b9feb1375cd7
TMWAG <semyon.valyaev@gmail.com>
2023-09-29 21:30:58 +0300

    добавлена ветка маршрутов /category

--- backend/src/modules/index.ts
+++ backend/src/modules/index.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
 import userRoutes from "./user/user.routes";
+import categoryRoutes from "./category/category.routes";
 
 export default async function routes(app: FastifyInstance) {
-  app.register(userRoutes, {prefix: '/user'});
+	app.register(userRoutes, { prefix: "/user" });
+	app.register(categoryRoutes, { prefix: "/category" });
 }
