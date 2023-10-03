d2745445625a742a3c35fab1ae529a832718e5f5
TMWAG <semyon.valyaev@gmail.com>
2023-10-03 16:24:46 +0300

    ходтверждение роли и статуса в системе по токену на стороне сервера

--- backend/src/decorators/auth.ts
+++ backend/src/decorators/auth.ts
@@ -2,25 +2,36 @@ import fastifyPlugin from "fastify-plugin"
 import { UserRoles } from "@prisma/client";
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { JwtPayload } from "../@types";
+import prisma from "../utils/prisma";
 
 async function authDecorators(app: FastifyInstance) {
   app.decorate('authenticate', async (req: FastifyRequest, rep: FastifyReply) => {
-    try {
-      await req.jwtVerify();
-    } catch (e) {
-      rep.code(401).send(e);
+    await req.jwtVerify();
+    const { id } = await req.jwtDecode<JwtPayload>();
+    if (!await prisma.user.findUnique({ where: { id } })){
+      return rep.code(404).send('Пользователь не найден')
     }
   });
   app.decorate('authorizeAdminOrSupervisor', async (req: FastifyRequest, rep: FastifyReply) => {
-    const { role } = await req.jwtDecode<JwtPayload>();
-    if (role === UserRoles.BUYER) {
-      rep.code(403).send('Недостаточно прав');
+    await req.jwtVerify();
+    const { id } = await req.jwtDecode<JwtPayload>();
+    const user = await prisma.user.findUnique({ where: { id } });
+    if (!user) {
+      return rep.code(404).send('Пользователь не найден');
+    }
+    if (user.role === UserRoles.BUYER) {
+      return rep.code(403).send('Недостаточно прав');
     }
   });
   app.decorate('authorizeAdmin',async (req: FastifyRequest, rep: FastifyReply) => {
-    const { role } = await req.jwtDecode<JwtPayload>();
-    if (role !== UserRoles.ADMIN) {
-      rep.code(403).send('Необходимы права администратора')
+    await req.jwtVerify();
+    const { id } = await req.jwtDecode<JwtPayload>();
+    const user = await prisma.user.findUnique({ where: { id } });
+    if (!user) {
+      return rep.code(404).send('Пользователь не найден');
+    }
+    if (user.role !== UserRoles.ADMIN) {
+      return rep.code(403).send('Необходимы права администратора')
     }
   });
 }
