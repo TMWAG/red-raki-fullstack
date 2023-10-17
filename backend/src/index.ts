283a38786ae6b146f6d0be4c4c449cc7823b0821
TMWAG <semyon.valyaev@gmail.com>
2023-10-17 15:10:27 +0300

    подключен плагин cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,13 +2,19 @@ import fastify from "fastify";
 import routes from "./modules";
 import fjwt from "@fastify/jwt";
 import auth from "./decorators/auth";
+import cors from "@fastify/cors";
 import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 
 export const app = fastify({
 	logger: true,
 }).withTypeProvider<TypeBoxTypeProvider>();
 
-app.register(fjwt, { secret: String(process.env.JWT_SALT) });
+app.register(fjwt, {
+	secret: String(process.env.JWT_SALT),
+});
+app.register(cors, {
+	origin: true,
+})
 
 app.register(auth);
 
