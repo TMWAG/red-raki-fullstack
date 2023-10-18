e6cbefd8a68377559e1c93e17055cac0520cb0bc
TMWAG <semyon.valyaev@gmail.com>
2023-10-18 17:05:08 +0300

    удалено поле image из схем, добавлены новые схемы и типы для создания, обновления и удвления

--- backend/src/modules/category/category.schema.ts
+++ backend/src/modules/category/category.schema.ts
@@ -1,9 +1,14 @@
 import { Static, Type } from "@sinclair/typebox";
 
+export const createCategorySchema = Type.Object({
+  name: Type.String(),
+});
+
+export type createCategoryInput = Static<typeof createCategorySchema>;
+
 export const createCategoryResponseSchema = Type.Object({
   id: Type.String(),
   name: Type.String(),
-  image: Type.String(),
 })
 
 export const getCategoriesResponseSchema = Type.Object({
@@ -11,18 +16,29 @@ export const getCategoriesResponseSchema = Type.Object({
   categories: Type.Array(Type.Object({
     id: Type.String(),
     name: Type.String(),
-    image: Type.String(),
   })),
 });
+
+export const updateCategoryBodySchema = Type.Object({
+  name: Type.String(),
+});
+export const updateCategoryParamsSchema = Type.Object({
+  id: Type.String(),
+});
+export type updateCategoryBodyInput = Static<typeof updateCategoryBodySchema>;
+export type updateCategoryParamsInput = Static<typeof updateCategoryParamsSchema>;
 
 export const updateCategoryResponseSchema = Type.Object({
   id: Type.String(),
   name: Type.String(),
-  image: Type.String(),
 });
 
 export const deleteCategorySchema = Type.Object({
   id: Type.String(),
 });
+
+export const deleteCategoryResponseSchema = Type.Object({
+  id: Type.String(),
+})
 
 export type deleteCategoryInput = Static<typeof deleteCategorySchema>;
