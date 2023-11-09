081119e2f5b31bfdca92c476435c54c5ef9d04bb
TMWAG <semyon.valyaev@gmail.com>
2023-11-09 19:59:06 +0300

    форма регистрации

---
+++ frontend/pages/register.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+useHead({
+	title: 'Регистрация'
+})
+</script>
+
+<template>
+  <div class="auth">
+    <RegisterForm />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.auth {
+	margin: 2rem auto;
+	width: 60%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+</style>
