b57c1567cdf70886ef5aab77b13b1f0f076bf3d2
TMWAG <semyon.valyaev@gmail.com>
2023-11-09 20:01:58 +0300

    страница входа

---
+++ frontend/pages/login.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+useHead({
+  title: 'Авторизация',
+})
+</script>
+
+<template>
+  <div class="auth">
+    <LoginForm/>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '/assets/colors';
+.auth {
+  margin: 2rem auto;
+  width: 60%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
