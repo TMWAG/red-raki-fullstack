1db8b2448e9d46ee69e0511964d7571477d6a3ce
TMWAG <semyon.valyaev@gmail.com>
2023-11-09 15:11:00 +0300

    модули pinia (и плагин), vueuse, переход для страниц, рантайм конфиг

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: [
+    '@pinia/nuxt',
+    '@pinia-plugin-persistedstate/nuxt',
+    '@vueuse/nuxt',
+  ],
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' },
+  },
+  runtimeConfig: {
+    public: {
+      apiUrl: process.env.API_URL,
+      container: process.env.CONTAINER,
+    },
+  },
 })
