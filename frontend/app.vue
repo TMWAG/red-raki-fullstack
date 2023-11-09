eb88409347749ae870ff19b506a49d29052ee100
TMWAG <semyon.valyaev@gmail.com>
2023-11-09 19:43:17 +0300

    задан темплейт для заголовков страниц, добавлен лейаут, index.scss и анимамция перехода между страницами

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,24 @@
+<script setup lang="ts">
+useHead({
+  titleTemplate: '%s | Красный Рак'
+})
+</script>
+
 <template>
-  <div>
-    hello world
-  </div>
+  <NuxtLayout name="default">
+    <NuxtPage/>
+  </NuxtLayout>
 </template>
+
+<style lang="scss">
+@import url('assets/index.scss');
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.2s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
