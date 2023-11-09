222bc0336ace4bc191c4f9ac2a9e010ea366547f
TMWAG <semyon.valyaev@gmail.com>
2023-11-09 23:23:01 +0300

    подключены маршруты заказов

--- backend/src/modules/index.ts
+++ backend/src/modules/index.ts
@@ -2,9 +2,11 @@ import { FastifyInstance } from "fastify";
 import userRoutes from "./user/user.routes";
 import categoryRoutes from "./category/category.routes";
 import productRoutes from "./product/product.routes";
+import orderRoutes from "./order/order.routes";
 
 export default async function routes(app: FastifyInstance) {
 	app.register(userRoutes, { prefix: "/user" });
 	app.register(categoryRoutes, { prefix: "/category" });
 	app.register(productRoutes, { prefix: "/product" });
+	app.register(orderRoutes, { prefix: "/order" });
 }
