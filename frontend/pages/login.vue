f024d6cdc093d4216dd94324172a9bacc9fc165a
TMWAG <semyon.valyaev@gmail.com>
2023-11-20 17:22:02 +0300

    изменены отступы

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -13,8 +13,8 @@ useHead({
 <style lang="scss" scoped>
 @use '/assets/colors';
 .auth {
-  margin: 2rem auto;
-  width: 60%;
+  padding-top: 55px;
+  margin: 0 auto;
   display: flex;
   align-items: center;
   justify-content: center;
