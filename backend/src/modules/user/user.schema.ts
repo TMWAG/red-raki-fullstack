fbfcd90180a80402f1c212cbec001ccb6380623a
TMWAG <semyon.valyaev@gmail.com>
2023-11-20 12:21:05 +0300

    изменено регулярное выражение для проверки номера телефона

--- backend/src/modules/user/user.schema.ts
+++ backend/src/modules/user/user.schema.ts
@@ -2,34 +2,36 @@ import { UserRoles } from "@prisma/client";
 import { Type, Static } from "@sinclair/typebox";
 
 export const createUserSchema = Type.Object({
-  phone: Type.RegExp(/^((\+7|7|8)+([0-9]){10})$/),
-	password: Type.String()
+	phone: Type.RegExp(/^\([0-9]{3}\)([0-9]){3}(-([0-9]){2}){2}/gm),
+	password: Type.String(),
 });
 export const createUserResponseSchema = Type.Object({
-  id: Type.String(),
-  phone: Type.String(),
-  role: Type.Enum(UserRoles),
+	id: Type.String(),
+	phone: Type.String(),
+	role: Type.Enum(UserRoles),
 });
 
 export const loginUserSchema = Type.Object({
-  phone: Type.String(),
-  password: Type.String(),
-})
+	phone: Type.String(),
+	password: Type.String(),
+});
 
 export const loginUserResponseSchema = Type.Object({
-  phone: Type.String(),
-  role: Type.Enum(UserRoles),
-  token: Type.String(),
-})
+	phone: Type.String(),
+	role: Type.Enum(UserRoles),
+	token: Type.String(),
+});
 
 export const getAllUsersResponseSchema = Type.Object({
-  count: Type.Integer(),
-  users: Type.Array(Type.Object({
-    id: Type.String(),
-    name: Type.Optional(Type.String()),
-    phone: Type.String(),
-    role: Type.Enum(UserRoles),
-  })),
+	count: Type.Integer(),
+	users: Type.Array(
+		Type.Object({
+			id: Type.String(),
+			name: Type.Optional(Type.String()),
+			phone: Type.String(),
+			role: Type.Enum(UserRoles),
+		})
+	),
 });
 
 export type CreateUserInput = Static<typeof createUserSchema>;
