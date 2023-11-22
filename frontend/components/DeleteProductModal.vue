f2341d70eb94e5bc9c19ec44b52b32de3bdd5a43
TMWAG <semyon.valyaev@gmail.com>
2023-11-22 19:18:50 +0300

    добавлен id для ввода

--- frontend/components/DeleteProductModal.vue
+++ frontend/components/DeleteProductModal.vue
@@ -16,21 +16,21 @@ const emit = defineEmits<{
 
 const delConfirmation = ref<string>("");
 const confirmationError = ref<string>("");
-const onConfirmationChange = (e: Event) => {
-	const { value } = e.target as HTMLInputElement;
-	if (value === props.product.name) {
-    confirmationError.value = "";
-  } else {
+function validateConfirmation() {
+	if (delConfirmation.value.length === 0) {
+		confirmationError.value = "Подтверждение необходимо указать";
+	} else if (delConfirmation.value !== props.product.name) {
 		confirmationError.value = "Подтверждение не совпадает";
+	} else {
+		confirmationError.value = "";
 	}
-	delConfirmation.value = value;
-};
+}
 
 const sendButtonDisabled = computed(() => {
-  return Boolean(!delConfirmation.value || confirmationError.value);
-})
+	return Boolean(!delConfirmation.value || confirmationError.value);
+});
 
-const onDelete = async () => {
+async function deleteProduct() {
 	const req = await $fetch(`${$backendUrl()}/api/product/${props.product.id}`, {
 		method: "DELETE",
 		headers: {
@@ -44,53 +44,82 @@ const onDelete = async () => {
 		);
 		emit("deleted");
 	}
-};
+}
 </script>
 
 <template>
-  <TheModal
-    :header="`Удаление товара: ${props.product.name}`"
-    @close="emit('canceled')"
-  >
-    <div class="form_wrapper">
-      <ValidateableInput
-        :value="delConfirmation"
-        :error="confirmationError"
-        @input="onConfirmationChange"
-        :placeholder="product.name"
-        type="text"
-        label="Подтверждение удаления"
-      />
-      <button
-        class="button"
-        @click.prevent="onDelete"
-        :disabled="sendButtonDisabled"
-      >Удалить</button>
-    </div>
-  </TheModal>
+	<TheModal
+		:header="`Удаление товара ${product.name}`"
+		@close="emit('canceled')"
+	>
+		<div class="delete-product-modal">
+			<ValidateableInput lg label="Подтверждение удаления" v-slot="s">
+				<input
+					:id="s.id"
+					type="text"
+					:placeholder="product.name"
+					class="labeled-input__input labeled-input__input_lg"
+					v-model="delConfirmation"
+					@input="validateConfirmation"
+					@focus="validateConfirmation"
+				/>
+			</ValidateableInput>
+			<span v-if="confirmationError" class="delete-product-modal__error">{{
+				confirmationError
+			}}</span>
+			<button
+				class="delete-product-modal__btn"
+				@click.prevent="deleteProduct"
+				:disabled="sendButtonDisabled"
+			>
+				Удалить
+			</button>
+		</div>
+	</TheModal>
 </template>
 
-<style lang="scss" scoped>
-@use '/assets/colors';
-.form_wrapper {
+<style lang="scss">
+@use "sass:color";
+.delete-product-modal {
 	display: flex;
 	flex-direction: column;
 	align-items: center;
 	padding: 1rem;
-}
-.button {
-	font-size: 1rem;
-	height: 2rem;
-	width: fit-content;
-	padding: 0 0.25rem;
-	border: 2px solid colors.$accent;
-	border-radius: 3px;
-	background-color: white;
-	transition: border-color, scale ease-in-out 0.3s;
-	&:hover {
-		scale: 105%;
-		border-color: colors.$primary;
-		transition: border-color, scale ease-in-out 0.3s;
+	&__btn {
+		width: 450px;
+		height: 48px;
+		border-radius: 8px;
+		background-color: #591c21;
+		outline: none;
+		border: none;
+		text-align: center;
+		font-family: "Raleway";
+		font-size: 20px;
+		font-style: normal;
+		font-weight: 600;
+		line-height: normal;
+		color: #fbfbfb;
+		transition: all ease 0.2s;
+		&:hover {
+			background-color: color.adjust($color: #591c21, $lightness: 5%);
+		}
+		&:focus {
+			outline: 1px solid #591c21;
+			outline-offset: 1px;
+		}
+		&:disabled {
+			background-color: #7B6063;
+			color: #FBFBFB80;
+		}
+	}
+	&__error {
+		color: #d40000;
+		font-family: "Raleway";
+		font-size: 15px;
+		font-style: normal;
+		font-weight: 600;
+		line-height: normal;
+		letter-spacing: 0.3px;
 	}
 }
 </style>
