7d6b987a408b9df256da6b219f077ac46434f945
TMWAG <semyon.valyaev@gmail.com>
2023-11-30 22:27:06 +0300

    убрвн скоупинг стилей

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -10,13 +10,5 @@ useHead({
   </div>
 </template>
 
-<style lang="scss" scoped>
-@use '/assets/colors';
-.auth {
-  padding-top: 55px;
-  margin: 0 auto;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
+<style lang="scss">
 </style>
