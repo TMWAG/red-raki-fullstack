7173167b2f494f3847146d82303235ccc9ba3b56
TMWAG <semyon.valyaev@gmail.com>
2023-11-30 18:17:22 +0300

    эндпоинт для получения всех заказов

--- backend/src/modules/order/order.routes.ts
+++ backend/src/modules/order/order.routes.ts
@@ -1,50 +1,67 @@
 import { FastifyInstance } from "fastify";
-import { createOrderInput, createOrderResponseSchema, createOrderSchema, getAllUserOrdersResponseSchema, getOrderInfoInput, getOrderInfoResponseSchema, getOrderInfoSchema } from "./order.schema";
-import { createOrderHandler, getAllUserOrdersHandler, getOrderInfoHandler, newOrdersHandler } from "./order.service";
+import {
+	createOrderInput,
+	createOrderResponseSchema,
+	createOrderSchema,
+	getOrderInfoInput,
+	getOrderInfoResponseSchema,
+	getOrdersByStatusInput,
+} from "./order.schema";
+import {
+	createOrderHandler,
+	getAllOrdersHandler,
+	getAllUserOrdersHandler,
+	getOrderInfoHandler,
+	newOrdersHandler,
+} from "./order.service";
 
 export default async function orderRoutes(app: FastifyInstance) {
-  app.post<{ Body: createOrderInput }>(
-    '/',
-    {
-      schema: {
-        body: createOrderSchema,
-        response: {
-          201: createOrderResponseSchema,
-        },
-      },
-    },
-    createOrderHandler,
-  );
-  app.get(
-    '/',
-    {
-      preHandler: [app.authenticate],
-      schema: {
-        response: {
-          200: getAllUserOrdersResponseSchema,
-        },
-      },
-    },
-    getAllUserOrdersHandler,
-  );
-  app.get<{ Params: getOrderInfoInput }>(
-    '/:id',
-    {
-      preHandler: [app.authenticate],
-      schema: {
-        params: getOrderInfoSchema,
-        response: {
-          200: getOrderInfoResponseSchema,
-        },
-      },
-    },
-    getOrderInfoHandler,
-  );
-  app.get(
-    '/manage',
-    {
-      preHandler: [app.authorizeAdminOrSupervisor],
-    },
-    newOrdersHandler,
-  );
+	app.post<{ Body: createOrderInput }>(
+		"/",
+		{
+			schema: {
+				body: createOrderSchema,
+				response: {
+					201: createOrderResponseSchema,
+				},
+			},
+		},
+		createOrderHandler
+	);
+	app.get<{ Querystring: getOrdersByStatusInput }>(
+		"/all",
+		{
+			preHandler: [app.authorizeAdminOrSupervisor],
+		},
+		getAllOrdersHandler
+	);
+	app.get(
+		"/my",
+		{
+			preHandler: [app.authenticate],
+			schema: {
+				response: {},
+			},
+		},
+		getAllUserOrdersHandler
+	);
+	app.get<{ Params: getOrderInfoInput }>(
+		"/id/:id",
+		{
+			preHandler: [app.authenticate],
+			schema: {
+				response: {
+					200: getOrderInfoResponseSchema,
+				},
+			},
+		},
+		getOrderInfoHandler
+	);
+	app.get(
+		"/manage",
+		{
+			preHandler: [app.authorizeAdminOrSupervisor],
+		},
+		newOrdersHandler
+	);
 }
