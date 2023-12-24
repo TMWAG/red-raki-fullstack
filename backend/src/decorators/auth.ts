2f62dda0cffc4d11729872c849d68da6f473502f
TMWAG <semyon.valyaev@gmail.com>
2023-12-24 11:29:05 +0300

    изменено название модели с user на employee

--- backend/src/decorators/auth.ts
+++ backend/src/decorators/auth.ts
@@ -1,5 +1,5 @@
 import fastifyPlugin from "fastify-plugin"
-import { UserRoles } from "@prisma/client";
+import { EmployeeRoles } from "@prisma/client";
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { JwtPayload } from "../@types";
 import prisma from "../utils/prisma";
@@ -8,29 +8,26 @@ async function authDecorators(app: FastifyInstance) {
   app.decorate('authenticate', async (req: FastifyRequest, rep: FastifyReply) => {
     await req.jwtVerify();
     const { id } = await req.jwtDecode<JwtPayload>();
-    if (!await prisma.user.findUnique({ where: { id } })){
+    if (!await prisma.employee.findUnique({ where: { id } })){
       return rep.code(404).send('Пользователь не найден')
     }
   });
   app.decorate('authorizeAdminOrSupervisor', async (req: FastifyRequest, rep: FastifyReply) => {
     await req.jwtVerify();
     const { id } = await req.jwtDecode<JwtPayload>();
-    const user = await prisma.user.findUnique({ where: { id } });
+    const user = await prisma.employee.findUnique({ where: { id } });
     if (!user) {
       return rep.code(404).send('Пользователь не найден');
-    }
-    if (user.role === UserRoles.BUYER) {
-      return rep.code(403).send('Недостаточно прав');
     }
   });
   app.decorate('authorizeAdmin',async (req: FastifyRequest, rep: FastifyReply) => {
     await req.jwtVerify();
     const { id } = await req.jwtDecode<JwtPayload>();
-    const user = await prisma.user.findUnique({ where: { id } });
+    const user = await prisma.employee.findUnique({ where: { id } });
     if (!user) {
       return rep.code(404).send('Пользователь не найден');
     }
-    if (user.role !== UserRoles.ADMIN) {
+    if (user.role !== EmployeeRoles.ADMIN) {
       return rep.code(403).send('Необходимы права администратора')
     }
   });
