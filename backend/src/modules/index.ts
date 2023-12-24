b4580b37879519150f781abcf083ea23b1465aae
TMWAG <semyon.valyaev@gmail.com>
2023-12-24 11:40:26 +0300

    выключен роут для заказов

--- backend/src/modules/index.ts
+++ backend/src/modules/index.ts
@@ -8,5 +8,5 @@ export default async function routes(app: FastifyInstance) {
 	app.register(userRoutes, { prefix: "/user" });
 	app.register(categoryRoutes, { prefix: "/category" });
 	app.register(productRoutes, { prefix: "/product" });
-	app.register(orderRoutes, { prefix: "/order" });
+	// app.register(orderRoutes, { prefix: "/order" });
 }
