a4f9992bad48d10a92a04adbbd766a5751576074
TMWAG <semyon.valyaev@gmail.com>
2023-12-27 16:26:49 +0300

    компонент для ввода текста

---
+++ frontend/components/UI/TextInput.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+withDefaults(
+	defineProps<{
+		label?: string;
+		placeholder?: string;
+		modelValue: string;
+	}>(),
+	{
+		label: "Логин",
+		placeholder: "login",
+	}
+);
+
+const emit = defineEmits<{
+	(e: "update:modelValue", value: string): void;
+}>();
+
+function updateValue(e: Event) {
+	emit("update:modelValue", (e.target as HTMLInputElement).value);
+}
+</script>
+
+<template>
+	<UIInputWrapper :label="label" v-slot="s">
+    <input
+      :id="s.id"
+      type="text"
+      :placeholder="placeholder"
+      :value="modelValue"
+      @input="updateValue"
+    />
+  </UIInputWrapper>
+</template>
+
+<style></style>
