49f713567495bd9c893986a50f04f52764a24500
TMWAG <semyon.valyaev@gmail.com>
2023-12-27 15:45:11 +0300

    компонент для отображения ошибок

---
+++ frontend/components/UI/ErrorList.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+defineProps<{
+	errorsList: (string | undefined)[];
+}>();
+</script>
+
+<template>
+	<div :class="$style.list">
+		<p
+			v-for="e in errorsList"
+			:key="`error-${e}`"
+			:class="$style.item"
+			v-show="e"
+		>
+			{{ e }}
+		</p>
+	</div>
+</template>
+
+<style module lang="scss">
+.list {
+	padding-left: 4px;
+	align-self: flex-start;
+	display: flex;
+	flex-direction: column;
+	gap: 3px;
+	align-items: flex-start;
+}
+.item {
+	display: block;
+	color: #d40000;
+	font-family: "Raleway";
+	font-size: 13px;
+	font-style: normal;
+	font-weight: 500;
+	line-height: normal;
+	letter-spacing: 0.26px;
+}
+</style>
