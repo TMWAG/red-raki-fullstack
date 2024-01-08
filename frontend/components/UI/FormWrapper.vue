763c3c3c5c2333977cfbc46b8b15962bbd512355
TMWAG <semyon.valyaev@gmail.com>
2024-01-08 22:03:20 +0300

    компонент-обёртка для формы

---
+++ frontend/components/UI/FormWrapper.vue
@@ -0,0 +1,15 @@
+<template>
+  <form :class="$style['form-wrapper']">
+    <slot/>
+  </form>
+</template>
+
+<style module lang="scss">
+.form-wrapper {
+	display: flex;
+	align-items: stretch;
+	flex-direction: column;
+	gap: 9px 0;
+	padding: 19px 19px 18px;
+}
+</style>
