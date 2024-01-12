e2f1a3e7ab77c1d78052be508357f1fc427beb6e
TMWAG <semyon.valyaev@gmail.com>
2024-01-12 16:53:26 +0300

    функиолнал просмотра, создания, измененя пароля пользователей

--- frontend/pages/users.vue
+++ frontend/pages/users.vue
@@ -25,6 +25,37 @@ function showAddModal() {
 function hideAddModal() {
 	isAddModalVisible.value = false;
 }
+
+const isChangePasswordModalVisible = ref<boolean>(false);
+const passwordChangeSuccess = ref<boolean>(false);
+function showChangePasswordModal() {
+	isChangePasswordModalVisible.value = true;
+}
+function hideChangePasswordModal() {
+	isChangePasswordModalVisible.value = false;
+}
+function changePassword(oldPassword: string, newPassword: string) {
+	$fetch(`${$backendUrl()}/api/user/change_password`, {
+		method: "PATCH",
+		body: JSON.stringify({
+			oldPassword,
+			newPassword,
+		}),
+		headers: {
+			Authorization: `Bearer ${useCookie("token").value}`,
+		},
+	})
+	.then(() => passwordChangeSuccess.value = true)
+	.catch((e) => {
+		console.log(e);
+	});
+}
+onMounted(() => {
+	if (useCookie("role").value !== "ADMIN") {
+		return navigateTo("/");
+	}
+});
+
 onUpdated(() => {
 	if (useCookie("role").value !== "ADMIN") {
 		return navigateTo("/");
@@ -39,14 +70,18 @@ useHead({
 	<div>
 		<h2>Пользователи</h2>
 		<button @click="showAddModal">Добавить нового пользователя</button>
-		<div>
-			<div v-for="u in data?.users" :key="u.id">
-				<p>{{ u.login }}</p>
-				<p>Имя: {{ u.name }}</p>
-				<p>Фамилия: {{ u.surname }}</p>
-			</div>
+		<button @click="showChangePasswordModal">Изменить пароль</button>
+		<div :class="$style.user_card_list">
+			<UIUserCard v-for="u in data?.users" :user="u" />
 		</div>
-		<AddUserModal
+		<div :class="$style.self_control"></div>
+		<ChangePasswordForm
+			v-show="isChangePasswordModalVisible"
+			:success="passwordChangeSuccess"
+			@canceled="hideChangePasswordModal"
+			@change-attempt="changePassword"
+		/>
+		<LazyFormUserAdd
 			v-if="isAddModalVisible"
 			@canceled="hideAddModal"
 			@created="updateUsers"
@@ -54,4 +89,13 @@ useHead({
 	</div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss" module>
+.user_card_list {
+	display: grid;
+	grid-template-columns: repeat(4, 1fr);
+	gap: 12px;
+}
+.self_control {
+	background-color: red;
+}
+</style>
