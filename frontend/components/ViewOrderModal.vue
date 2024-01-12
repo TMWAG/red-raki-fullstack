8dbce4936ca8bea8f7a1221c6a08f2f9b17fa3ff
TMWAG <semyon.valyaev@gmail.com>
2024-01-12 16:43:59 +0300

    заготовка для формы просмотра заказа

---
+++ frontend/components/ViewOrderModal.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { IOrderResponse } from "~/@types";
+
+defineProps<{
+	order: IOrderResponse;
+}>();
+
+const emit = defineEmits<{
+	(e: "closed"): void;
+}>();
+
+function onModalClose() {
+	emit("closed");
+}
+</script>
+
+<template>
+	<TheModal header="Информация о заказе" @close="onModalClose">
+		<div class="view-order-modal">
+      {{ order }}
+    </div>
+	</TheModal>
+</template>
+
+<style lang="scss">
+.view-order-modal {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  padding: 10px;
+}
+</style>
