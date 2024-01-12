aad901529c7c4537aca4ba9ef1eed917a43026e9
TMWAG <semyon.valyaev@gmail.com>
2024-01-12 16:55:14 +0300

    заготовка для страницы личнного кабинета

---
+++ frontend/pages/me.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const { $backendUrl } = useNuxtApp();
+const { data } = await useFetch(`${$backendUrl()}/api/order/my`, {
+	method: "get",
+	key: "myOrders",
+	headers: {
+		Authorization: `Bearer ${useCookie("token").value}`,
+	},
+});
+
+
+
+useHead({
+	title: "Личный кабинет",
+});
+</script>
+
+<template>
+	<div>
+		<NuxtLayout name="profile-layout">
+			<pre>{{ data }}</pre>
+		</NuxtLayout>
+	</div>
+</template>
+
+<style lang="scss"></style>
